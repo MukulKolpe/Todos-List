c50d5d9c11ce1f4e503cafafd894743684958dda
Mukul <mukulkolpe45@gmail.com>
2021-05-06 01:10:09 +0530

    Updated TodoItem.js

--- src/MyComponents/TodoItem.js
+++ src/MyComponents/TodoItem.js
@@ -4,7 +4,7 @@ export const TodoItem = ({todo, onDelete}) => {
     return (
         <>
         <div>
-            <h4 className="text-center my-3">{todo.title}</h4>
+            <h4>{todo.title}</h4>
             <p>{todo.desc}</p>
             <button className="btn btn-sm btn-danger" onClick={()=>{onDelete(todo)}}>Delete</button>
         </div>
