9a3a0b318b47becba18beb22ae4df55febd91b7c
Mukul <mukulkolpe45@gmail.com>
2021-05-05 13:59:57 +0530

    Updates about section

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -4,12 +4,9 @@ export const About = () => {
     return (
         <div>
             This is an about component
-            <p>Lorem ipsum dolor sit amet consectetur,
-               adipisicing elit. Ratione, totam. Molestias
-               rerum accusamus, excepturi vitae quidem 
-               explicabo esse ex iusto iste consectetu
-               r veritatis ipsam, dicta non exercitationem
-               neque amet ut!</p>
+            <p>Hey this is about section of this Todos-List. You can create your Todos here. This react web-App is hosted on netlify.
+                Thank you for visiting!!
+            </p>
         </div>
     )
 }
