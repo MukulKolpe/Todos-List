ac7859742b5015304851816168ec42c713b7e17c
Mukul <mukulkolpe45@gmail.com>
2021-05-05 12:47:42 +0530

    Todos-List Done

---
+++ src/MyComponents/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>Lorem ipsum dolor sit amet consectetur,
+               adipisicing elit. Ratione, totam. Molestias
+               rerum accusamus, excepturi vitae quidem 
+               explicabo esse ex iusto iste consectetu
+               r veritatis ipsam, dicta non exercitationem
+               neque amet ut!</p>
+        </div>
+    )
+}
