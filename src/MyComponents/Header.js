c524761f631c2ac531bf51e6f34c23537276e264
Mukul <mukulkolpe45@gmail.com>
2021-05-06 00:43:20 +0530

    update in Header.js

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -26,7 +26,7 @@ export default function Header(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-Link active me-2" aria-current="page" to="/">
+                <Link className="nav-Link active" aria-current="page" to="/">
                   Home
                 </Link>
               </li>
