aa6ed02e4795d7b5e5348cb307f4b51ba1d3fc52
Mukul <mukulkolpe45@gmail.com>
2021-08-17 23:29:10 +0530

    social media icons and links added

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -5,7 +5,42 @@ export const Footer = () => {
     
   return (
     <footer className="footer">
-      <p className="footer-content">Copyright &copy; Mukul Kolpe</p>
+      
+      <a
+        href="https://www.linkedin.com/in/mukulkolpe/"
+        className="footer-link"
+        target="_blank"
+        rel="noreferrer"
+      >
+       <i class="fa fa-linkedin"></i>
+      </a>
+      <a
+        href="https://twitter.com/MukulKolpe"
+        className="footer-link"
+        target="_blank"
+        rel="noreferrer"
+      >
+       <i class="fa fa-twitter"></i>
+      </a>
+
+      <a
+        href="https://github.com/MukulKolpe"
+        className="footer-link"
+        target="_blank"
+        rel="noreferrer"
+      >
+       <i class="fa fa-github"></i>
+      </a>
+
+      <a
+        href="https://www.instagram.com/mukul_kolpe/?hl=en"
+        className="footer-link"
+        target="_blank"
+        rel="noreferrer"
+      >
+       <i class="fa fa-instagram"></i>
+      </a>
+      
     </footer>
   );
 };
